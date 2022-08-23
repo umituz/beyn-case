2a216f6efb4eb923461a785ca6638ab8e8d3c3a7
Ümit UZ <umituz998@gmail.com>
2022-08-24 01:29:15 +0300

    .

--- app/Repositories/CarRepository.php
+++ app/Repositories/CarRepository.php
@@ -3,13 +3,35 @@
 namespace App\Repositories;
 
 use App\Models\Car;
+use Exception;
+use Illuminate\Support\Facades\DB;
 
 class CarRepository implements CarRepositoryInterface
 {
+    /**
+     * @var Car
+     */
     private Car $car;
 
+    /**
+     * @param Car $car
+     */
     public function __construct(Car $car)
     {
         $this->car = $car;
+    }
+
+    /**
+     * @return false|mixed
+     */
+    public function getAll(): mixed
+    {
+        try {
+            return DB::transaction(function () {
+                return Car::paginate();
+            });
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
--- app/Repositories/CarRepositoryInterface.php
+++ app/Repositories/CarRepositoryInterface.php
@@ -2,7 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\Car;
+
 interface CarRepositoryInterface
 {
+    /**
+     * @param Car $car
+     */
+    public function __construct(Car $car);
 
+    /**
+     * @return mixed
+     */
+    public function getAll(): mixed;
 }
