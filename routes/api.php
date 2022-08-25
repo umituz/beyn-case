a056ba822d4048f9460744ac28bfcea2c637fc61
Ümit UZ <umituz998@gmail.com>
2022-08-25 03:16:17 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
-
+    Route::post('logout', [AuthController::class, 'logout']);
 
     Route::prefix('v1')->group(function () {
         Route::get('users', [UsersController::class, 'index']);
