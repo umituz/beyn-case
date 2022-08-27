7a7eccb3b2bb88494f2fe7ffe0b24a6c897704d5
Ümit UZ <umituz998@gmail.com>
2022-08-28 02:50:02 +0300

    .

--- app/Traits/NotifiableOnSlack.php
+++ app/Traits/NotifiableOnSlack.php
@@ -14,14 +14,6 @@ trait NotifiableOnSlack
     use Notifiable;
 
     /**
-     * @return string|null
-     */
-    public function routeNotificationForSlack(): ?string
-    {
-        return config('slack.endpoint');
-    }
-
-    /**
      * @param  string  $channel
      * @param  string  $message
      * @param  string|null  $from
@@ -30,15 +22,5 @@ trait NotifiableOnSlack
     public function toSlack(string $channel, string $message, ?string $from = null): void
     {
         $this->notify(new Slack($channel, $message, $from ?? config('slack.username')));
-    }
-
-    /**
-     * @param string $channel
-     * @param string $view
-     * @param array $parameters
-     */
-    public function toSlackFromView(string $channel, string $view, array $parameters)
-    {
-        $this->notify(new Slack($channel, view($view, $parameters)->render(), config('slack.username')));
     }
 }
