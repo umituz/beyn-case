5a08798ed44e5f958c44fb7c7678bb0d58ffd054
Ümit UZ <umituz998@gmail.com>
2022-08-30 10:56:04 +0300

    .

--- app/Http/Controllers/Api/Brand/BrandsV1Controller.php
+++ app/Http/Controllers/Api/Brand/BrandsV1Controller.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\Api\Brand;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\ApiController;
 use App\Http\Requests\V1\Brand\BrandRequest;
 use App\Http\Resources\V1\Brand\BrandCollection;
 use App\Http\Resources\V1\Brand\BrandResource;
 use App\Repositories\BrandRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 
 /**
  * Class BrandsV1Controller
  * @package App\Http\Controllers\Api\Brand
  */
-class BrandsV1Controller extends Controller
+class BrandsV1Controller extends ApiController
 {
     private BrandRepositoryInterface $brandRepository;
 
@@ -27,39 +28,39 @@ class BrandsV1Controller extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return BrandCollection
+     * @return JsonResponse
      */
-    public function index(): BrandCollection
+    public function index(): JsonResponse
     {
         $brands = $this->brandRepository->getAll();
 
-        return new BrandCollection($brands);
+        return $this->success(__('Success'),  new BrandCollection($brands));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param BrandRequest $request
-     * @return BrandResource
+     * @return JsonResponse
      */
-    public function store(BrandRequest $request): BrandResource
+    public function store(BrandRequest $request): JsonResponse
     {
         $brand = $this->brandRepository->create($request->validated());
 
-        return new BrandResource($brand);
+        return $this->success(__('Success'),  new BrandResource($brand));
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return BrandResource
+     * @return JsonResponse
      */
-    public function show($id): BrandResource
+    public function show($id): JsonResponse
     {
         $brand = $this->brandRepository->getById($id);
 
-        return new BrandResource($brand);
+        return $this->success(__('Success'),  new BrandResource($brand));
     }
 
     /**
@@ -67,27 +68,27 @@ class BrandsV1Controller extends Controller
      *
      * @param BrandRequest $request
      * @param int $id
-     * @return BrandResource
+     * @return JsonResponse
      */
-    public function update(BrandRequest $request, $id): BrandResource
+    public function update(BrandRequest $request, $id): JsonResponse
     {
         $brand = $this->brandRepository->update($id, $request->validated());
 
-        return new BrandResource($brand);
+        return $this->success(__('Success'),  new BrandResource($brand));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return array
+     * @return JsonResponse
      */
-    public function destroy($id): array
+    public function destroy($id): JsonResponse
     {
         $this->brandRepository->delete($id);
 
-        return [
-            'message' => __('Deleted')
-        ];
+        return $this->success(__('Success'), [
+            'message' => 'Deleted'
+        ]);
     }
 }
